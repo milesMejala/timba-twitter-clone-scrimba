aa9c993f23b9f17d61687d1c4eee0a69bc6ab4b5
milesMejala <141707133+milesMejala@users.noreply.github.com>
2025-04-18 14:29:42 +0800

    Update index.js
    new feature - user can reply to tweets (incomplete)

--- index.js
+++ index.js
@@ -93,54 +93,59 @@ function getFeedHtml(){
         if(tweet.replies.length > 0){
             tweet.replies.forEach(function(reply){
                 repliesHtml+=`
-<div class="tweet-reply">
-    <div class="tweet-inner">
-        <img src="${reply.profilePic}" class="profile-pic">
-            <div>
-                <p class="handle">${reply.handle}</p>
-                <p class="tweet-text">${reply.tweetText}</p>
-            </div>
-        </div>
-</div>
-`
+                            <div class="tweet-reply">
+                                <div class="tweet-inner">
+                                    <img src="${reply.profilePic}" class="profile-pic">
+                                    <div>
+                                        <p class="handle">${reply.handle}</p>
+                                        <p class="tweet-text">${reply.tweetText}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            `
             })
         }
         
+        repliesHtml += `<div class="tweet-input-area tweet-reply">
+				            <img src="images/scrimbalogo.png" class="profile-pic">
+				            <textarea class="tweet-text user-reply" placeholder="Post your reply" id="reply-input"></textarea>
+			            </div>
+                        <button class="reply-btn tweet-text" id="reply-btn">Reply</button>`
           
         feedHtml += `
-<div class="tweet">
-    <div class="tweet-inner">
-        <img src="${tweet.profilePic}" class="profile-pic">
-        <div>
-            <p class="handle">${tweet.handle}</p>
-            <p class="tweet-text">${tweet.tweetText}</p>
-            <div class="tweet-details">
-                <span class="tweet-detail">
-                    <i class="fa-regular fa-comment-dots"
-                    data-reply="${tweet.uuid}"
-                    ></i>
-                    ${tweet.replies.length}
-                </span>
-                <span class="tweet-detail">
-                    <i class="fa-solid fa-heart ${likeIconClass}"
-                    data-like="${tweet.uuid}"
-                    ></i>
-                    ${tweet.likes}
-                </span>
-                <span class="tweet-detail">
-                    <i class="fa-solid fa-retweet ${retweetIconClass}"
-                    data-retweet="${tweet.uuid}"
-                    ></i>
-                    ${tweet.retweets}
-                </span>
-            </div>   
-        </div>            
-    </div>
-    <div class="hidden" id="replies-${tweet.uuid}">
-        ${repliesHtml}
-    </div>   
-</div>
-`
+                    <div class="tweet">
+                        <div class="tweet-inner">
+                            <img src="${tweet.profilePic}" class="profile-pic">
+                            <div>
+                                <p class="handle">${tweet.handle}</p>
+                                <p class="tweet-text">${tweet.tweetText}</p>
+                                <div class="tweet-details">
+                                    <span class="tweet-detail">
+                                        <i class="fa-regular fa-comment-dots"
+                                        data-reply="${tweet.uuid}"
+                                        ></i>
+                                        ${tweet.replies.length}
+                                    </span>
+                                    <span class="tweet-detail">
+                                        <i class="fa-solid fa-heart ${likeIconClass}"
+                                        data-like="${tweet.uuid}"
+                                        ></i>
+                                        ${tweet.likes}
+                                    </span>
+                                    <span class="tweet-detail">
+                                        <i class="fa-solid fa-retweet ${retweetIconClass}"
+                                        data-retweet="${tweet.uuid}"
+                                        ></i>
+                                        ${tweet.retweets}
+                                    </span>
+                                </div>   
+                            </div>            
+                        </div>
+                        <div class="hidden" id="replies-${tweet.uuid}">
+                            ${repliesHtml}
+                        </div>   
+                    </div>
+                    `
    })
    return feedHtml 
 }
